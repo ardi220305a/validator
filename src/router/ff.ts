21dabdb2a8e3aca92ca73e6c5af7b04096b3f21c
ardi220305a <156838606+ardi220305a@users.noreply.github.com>
2025-08-02 01:58:32 +0700

    Update ff.ts

--- src/router/ff.ts
+++ src/router/ff.ts
@@ -2,11 +2,23 @@ import { hitCoda, Result } from '../utils'
 
 export default async function ff(id: number): Promise<Result> {
   const body = `voucherPricePoint.id=1032429&voucherPricePoint.price=1110&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=FREEFIRE&shopLang=id_ID`
+  
   const data = await hitCoda(body)
+
+  if (data.errorCode && data.errorCode !== '') {
+    return {
+      success: false,
+      game: 'Garena Free Fire',
+      id,
+      name: '',
+      message: data.errorMsg || 'User ID tidak ditemukan'
+    }
+  }
+
   return {
     success: true,
     game: 'Garena Free Fire',
     id,
-    name: data.confirmationFields.roles[0].role
+    name: data.confirmationFields?.roles?.[0]?.role || ''
   }
 }
