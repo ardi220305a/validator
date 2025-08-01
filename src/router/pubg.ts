8cfb58ce3212bb79c39dfb31b0eb47efc8175e73
ardi220305a <156838606+ardi220305a@users.noreply.github.com>
2025-08-02 04:39:42 +0700

    Update pubg.ts

--- src/router/pubg.ts
+++ src/router/pubg.ts
@@ -4,17 +4,17 @@ import { hitCoda, Result } from '../utills'
 export default async function pb(id: number): Promise<Result> {
   const body = `category_id=59c0f241-e81b-482d-a7c4-43458d34fa1b&user.userId=${id}`
   const data = await hitCoda(body)
-  if (data.confirmationFields.roles[0].role) {
+  if (data.data) {
     return {
-      success: true,
-      game: 'Free Fire',
+      statusCode: 201,
+      game: 'PUBG',
       id,
-      name: data.confirmationFields.roles[0].role
+      name: data.data
     }
   }
   else {
     return {
-      success: false,
+      statusCode: 404,
       message: 'Not found'
     }
   }
