51836fac1eb718e399b9a23dbfbe2cb864b65414
ardi220305a <156838606+ardi220305a@users.noreply.github.com>
2025-08-02 04:22:57 +0700

    Create pubg.ts

---
+++ src/router/pubg.ts
@@ -0,0 +1,20 @@
+import { hitCoda, Result } from '../utils'
+
+export default async function pb(id: number): Promise<Result> {
+  const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=FREEFIRE&shopLang=id_ID`
+  const data = await hitCoda(body)
+  if (data.confirmationFields.roles[0].role) {
+    return {
+      success: true,
+      game: 'Free Fire',
+      id,
+      name: data.confirmationFields.roles[0].role
+    }
+  }
+  else {
+    return {
+      success: false,
+      message: 'Not found'
+    }
+  }
+}
