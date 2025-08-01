2aae9c1369fbe0aca4782a2304fa852398b9e969
ardi220305a <156838606+ardi220305a@users.noreply.github.com>
2025-08-02 04:44:48 +0700

    Update pubg.ts

--- src/router/pubg.ts
+++ src/router/pubg.ts
@@ -1,7 +1,7 @@
 
 import { hitCoda, Result } from '../utills'
 
-export default async function pb(id: number): Promise<Result> {
+export default async function pubg(id: number): Promise<Result> {
   const body = `category_id=59c0f241-e81b-482d-a7c4-43458d34fa1b&user.userId=${id}`
   const data = await hitCoda(body)
   if (data.data) {
