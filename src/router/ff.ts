b2069f9294f897650b8e50be5b33effb15681d89
ardi220305a <156838606+ardi220305a@users.noreply.github.com>
2025-08-02 02:32:47 +0700

    Update ff.ts

--- src/router/ff.ts
+++ src/router/ff.ts
@@ -3,16 +3,6 @@ import { hitCoda, Result } from '../utils'
 export default async function pb(id: number): Promise<Result> {
   const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=FREEFIRE&shopLang=id_ID`
   const data = await hitCoda(body)
-  if (data.confirmationFields.username) {
-    return {
-      success: true,
-      game: 'Free Fire',
-      id,
-      name: import { hitCoda, Result } from '../utils'
-
-export default async function pb(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=8050&voucherPricePoint.price=1000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=FREEFIRE&shopLang=id_ID`
-  const data = await hitCoda(body)
   if (data.confirmationFields.roles[0].role) {
     return {
       success: true,
