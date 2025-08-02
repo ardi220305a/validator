c4fd7779d105fe0d4135085c1800240d38696d9b
ardi220305a <156838606+ardi220305a@users.noreply.github.com>
2025-08-02 13:26:41 +0700

    Update pubg.ts

--- src/router/pubg.ts
+++ src/router/pubg.ts
@@ -1,6 +1,6 @@
 import { hitCoda, Result } from '../utills'
 
-export default async function pb(invoice_data: any, invoice_product: string, payment_method: string, use_point: boolean, voucher_code: string | null): Promise<Result> {
+export default async function pubg(invoice_data: any, invoice_product: string, payment_method: string, use_point: boolean, voucher_code: string | null): Promise<Result> {
   // Buat body form-urlencoded sesuai payload
   const body = `user_id=${invoice_data.user_id}&whatsapp=${invoice_data.whatsapp}&nickname=${invoice_data.nickname}&invoice_product=${invoice_product}&payment_method=${payment_method}&use_point=${use_point}&voucher_code=${voucher_code ?? ''}`
 
